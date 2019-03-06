56bb5a0445d7f865eafa6f864996c51c21bc2b08
Apoorva Ambhoj <apoorva.ambhoj95@gmail.com>
2019-03-06 20:25:39 +0530

    Add files via upload

---
+++ hostnameFromVmPayload.js
@@ -0,0 +1,41 @@
+if(payload != null) {
+                var sortKeys = payload.keys.sort()
+                sortKeys.forEach(function(key){
+                                logProperties(key, payload.get(key), "");
+                });
+} else { 
+                System.debug("Payload is null");
+}
+
+function logProperties(keyName, keyValue, keyParent) {
+                var keyValueType = System.getObjectType(keyValue);
+                if(keyValueType == "Properties") {
+                                var sortKeys = keyValue.keys.sort()
+                                sortKeys.forEach(function(k){
+                                                logProperties(k, keyValue.get(k), keyParent+keyName+".");
+                                });
+                } else {
+                                if(keyValue == "") {
+                                                keyValue = "null";
+                                }
+                                System.debug(keyParent + keyName + ":"+keyValueType+": " +keyValue);
+                }
+}
+System.log("--------------------------------------------------------------------")
+System.log(payload);
+System.log("--------------------------------------------------------------------")
+var  machine = payload.get("machine")
+System.log("--------------------------------------------------------------------")
+var vmProperties = machine.get("properties")
+System.log(vmProperties);
+VMName= machine.get("name")
+
+
+Hostname = VMName
+System.log("Hostname is---->"+Hostname)
+vms = Server.findAllForType("VC:VirtualMachine",Hostname);
+vm = vms[0]
+System.log(vm.name)
+ipAddr = vm.ipAddress
+
+
