5ae4230a1fd99e734acce6b8fc42c5e0f74a9b08
Apoorva Ambhoj <apoorva.ambhoj95@gmail.com>
2019-03-25 13:06:07 +0530

    Add files via upload

---
+++ getDateTime.js
@@ -0,0 +1,9 @@
+var x= new Date();
+var date = x.getDate();
+System.log(x);
+System.log("date is: "+ date);
+System.log(x.toString());
+var month= x.getMonth();
+System.log("month is: " + month);
+var hours= x.getHours();
+System.log("hours are: "+ hours
